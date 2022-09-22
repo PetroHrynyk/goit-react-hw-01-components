769222c129f604b75557a15f0015267d3a0af542
PetroHrynyk <grynyk82@gmail.com>
2022-09-22 19:27:19 +0300

    profile

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,16 @@
+import user from '../data/user.json';
+import Profile from './profile/Profile.jsx';
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <Profile
+      // username={user.username}
+      // tag={user.tag}
+      // location={location}
+      // avatar={avatar}
+      // followers={stats.followers}
+      // views={stats.views}
+      // likes={stats.likes}
+      user={user}
+    />
   );
 };
